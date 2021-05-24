3648fee1c3d9755445082c293031078f07effbd6
rohitkolankar <rohitkolankar7@gmail.com>
2021-05-24 13:28:34 +0530

    Initial Commit

---
+++ flashtype/src/Components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './../Nav/Nav.jsx'
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <Nav />
+            </div>
+        );
+    }
+}
+
+export default App;
