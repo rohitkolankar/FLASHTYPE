b84106cbdcd313b47f3201c4fa451b22b64e3dd4
rohitkolankar <rohitkolankar7@gmail.com>
2021-05-27 11:32:25 +0530

    Navbar changed

--- flashtype/src/Components/Nav/Nav.jsx
+++ flashtype/src/Components/Nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from './../../assets/logo.png';
 
 const Nav = () => {
     return (
         <div>
-            <h1>Nav bar</h1>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />
+      FLASHTYPE
+    </Navbar.Brand>
+            </Navbar>
 
         </div>
     );
--- flashtype/src/index.js
+++ flashtype/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App.jsx';
 import reportWebVitals from './reportWebVitals';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
